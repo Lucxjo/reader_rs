b9c0d43f89af0f4fb7479598cb5f6cd4f6c57d37
Louis Hollingworth <louis@hollingworth.ch>
2023-04-07 11:58:44 +0100

    Now fetches request from a URL
    Signed-off-by: Louis Hollingworth <louis@hollingworth.ch>

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,19 @@
+
+
 fn main() {
-    println!("Hello, world!");
+    run().unwrap_or(());
+
+    ()
+}
+
+#[tokio::main]
+async fn run() -> Result<(), Box<dyn std::error::Error>> {
+    let body = reqwest::get("https://www.reddit.com/r/YoungRoyals.rss")
+        .await?
+        .text()
+        .await?;
+
+    println!("body = {:?}", body);
+
+    Ok(())
 }
